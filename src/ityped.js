b8db09a87c863ca4d492e0657ec7c0d5bee29c3a
AliceAAtalla <aliceatalla36@gmail.com>
2020-09-29 21:08:18 -0300

    Iniciando o iTyped

---
+++ src/ityped.js
@@ -0,0 +1,10 @@
+ityped.init('#ityped', {
+  strings: [
+    'Dead simple animated typed.',
+    'No jQuery dependency.',
+    'Tiny size: <3kb.',
+    'Just install and Enjoy!',
+  ],
+  startDelay: 200,
+  loop: true,
+});
