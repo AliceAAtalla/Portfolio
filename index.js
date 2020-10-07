b34d0820c9e04d78207c935472bb1d89d3609c1f
AliceAAtalla <aliceatalla36@gmail.com>
2020-10-07 09:32:56 -0300

    Aplicando nodejs para formaulario de contato

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-document.querySelector('#hamburguer').addEventListener('click', () => {
-  const menu = document.querySelector('#menu');
-  const actived = document.querySelector('.active');
-  if (actived) {
-    return menu.classList.remove('active');
-  }
-  menu.classList.add('active');
+const express = require('express');
+const path = require('path');
+const sendEmail = require('./services/mail.js');
+
+const app = express();
+
+const PORT = 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.post('/', (req, res) => {
+  const { name, email, text } = req.body;
+  sendEmail(name, email, text, (err, data) => {
+    err ? res.status(500).json({ message: 'Internal Error' }) : res.redirect('/');
+  });
+});
+
+app.listen(PORT, () => {
+  console.log('Server is starting on PORT: ', 8080);
 });
