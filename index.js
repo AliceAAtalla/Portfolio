bdcc390d225dc746c58123ad0ce0ab5c053a1af5
AliceAAtalla <aliceatalla36@gmail.com>
2020-10-09 22:16:15 -0300

    Validaçao do formulario

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const sendEmail = require('./services/mail.js');
 
+const isValid = require('./services/validation');
+
 const app = express();
 
 const PORT = 8080;
@@ -15,12 +17,24 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'views', 'index.html'));
 });
 
-app.post('/', (req, res) => {
-  const { name, email, text } = req.body;
-  sendEmail(name, email, text, (err, data) => {
-    err ? res.status(500).json({ message: 'Internal Error' }) : res.redirect('/');
-  });
-});
+app.post(
+  '/',
+  (req, res, next) => {
+    const valid = isValid(req.body);
+
+    if (valid.status === 'Not Valid') {
+      return res.status(500).send(`<h1>${valid.message}<h1>`);
+    }
+    return next();
+  },
+  (req, res) => {
+    const { name, email, text } = req.body;
+
+    sendEmail(name, email, text, (err, data) => {
+      err ? res.status(500).json({ message: 'Internal Error' }) : res.redirect('/');
+    });
+  }
+);
 
 app.listen(PORT, () => {
   console.log('Server is starting on PORT: ', 8080);
---
+++ services/validation.js
@@ -0,0 +1,28 @@
+const emailValidator = /[A-Z0-9]{1,}@[A-Z0-9]{2,}\.[A-Z0-9]{2,}/i;
+const nameValidator = /\w{3,}/;
+
+const isValid = ({ name, email, text }) => {
+  console.log({ name, email, text });
+  if (!nameValidator.test(name)) {
+    return {
+      status: 'Not Valid',
+      message:
+        'Nome inválido. Digite um nome com apenas caracteres e com no minímo 3 letras',
+    };
+  }
+  if (!emailValidator.test(email)) {
+    return {
+      status: 'Not Valid',
+      message: 'Email inválido. Digite um email no formato email@examplo.com',
+    };
+  }
+  if (text.length < 100) {
+    return {
+      status: 'Not Valid',
+      message: 'Mensagem inválida. Digite uma mensagem com no minímo 100 caracteres',
+    };
+  }
+  return { status: 'Valid' };
+};
+
+module.exports = isValid;
